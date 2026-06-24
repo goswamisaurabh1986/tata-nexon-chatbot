67dc4f059a907576ce022b3f69e3649b401687a3
Saurabh <goswamisaurabh.1986@gmail.com>
2026-06-24 12:20:52 +0530

    Allow EC2 frontend CORS origin

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,8 @@ class AppSettings(BaseSettings):
             "http://localhost:5173",
             "http://localhost:8000",
             "http://127.0.0.1:5173",
+            "http://34.222.215.141:5173",
+            "http://34.222.215.141",
         ),
         validation_alias="CORS_ALLOWED_ORIGINS",
     )
